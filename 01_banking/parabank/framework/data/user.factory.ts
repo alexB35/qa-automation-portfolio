543f472217d71aea96464139abc2017be27bc5d5
alexB35 <alex.b35@live.fr>
2026-05-25 15:19:42 +0200

    update

--- 01_banking/parabank/framework/data/user.factory.ts
+++ 01_banking/parabank/framework/data/user.factory.ts
@@ -3,7 +3,7 @@ import { UserBase } from './user.base';
 // ── Fresh user creation data ──────────────────────────────────────────────────────────────
 export function buildUser(overrides?: Partial<UserBase>): UserBase {
   const unique = Math.random().toString(36).slice(2, 8);
-
+/*
   return {
     firstName: `John${unique}`,
     lastName:  `Doe${unique}`,
@@ -13,6 +13,21 @@ export function buildUser(overrides?: Partial<UserBase>): UserBase {
     zipCode:   '10001',
     phone:     '0123456789',
     ssn:       `${unique.slice(0,3)}-${unique.slice(3,5)}-${unique.slice(5)}`,
+    username:  `tu_${unique}`,
+    password:  'Test123!',
+    ...overrides,
+  };
+} */
+
+return {
+    firstName: `John`,
+    lastName:  `Doe`,
+    address:   `123 Main St`,
+    city:      'New York',
+    state:     'NY',
+    zipCode:   '10001',
+    phone:     '0123456789',
+    ssn:       `123-45-6789`,
     username:  `tu_${unique}`,
     password:  'Test123!',
     ...overrides,
