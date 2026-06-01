c72b503c3bc74a91d9e797ad2f2c41a00142f312
alexB35 <alex.b35@live.fr>
2026-06-01 15:13:33 +0200

    update

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 const DIRS = {
   'parabank': ['./01_banking/parabank/outputs/allure-results', './01_banking/parabank/outputs/allure-report'],
-  'restful-booker': ['./02_api/restful-booker/outputs/allure-results', './02_api/restful-booker/outputs/allure-report'],
+  'restful-booker': ['./02_api/restful_booker/outputs/allure-results', './02_api/restful-booker/outputs/allure-report'],
   'automation-exercise': ['./03_ecommerce/automation-exercise/outputs/allure-results', './03_ecommerce/automation-exercise/outputs/allure-report'],
   'all': [
     './allure-results', './allure-report', 
     './01_banking/parabank/outputs/allure-results',
     './01_banking/parabank/outputs/allure-report',
-    './02_api/restful-booker/outputs/allure-results',
-    './02_api/restful-booker/outputs/allure-report',
+    './02_api/restful_booker/outputs/allure-results',
+    './02_api/restful_booker/outputs/allure-report',
     './03_ecommerce/automation-exercise/outputs/allure-results',
     './03_ecommerce/automation-exercise/outputs/allure-report',
   ],
