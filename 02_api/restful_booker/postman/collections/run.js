e2ff66705998eabeb4387e164f7ff785bed7388d
alexB35 <alex.b35@live.fr>
2026-06-01 15:32:35 +0200

    update

--- 02_api/restful_booker/postman/collections/run.js
+++ 02_api/restful_booker/postman/collections/run.js
@@ -2,7 +2,6 @@ const newman = require('newman');
 const path = require('path');
 
 const envFile = '/tmp/rfb_env.json';
-
 const base = '02_api/restful_booker/postman';
 
 function run(collection, environment) {
@@ -29,19 +28,17 @@ function run(collection, environment) {
   try {
     const envInitial = `${base}/environments/RFB_Environment.postman_environment.json`;
 
-    const c1 = `${base}/collections/RFB-00_Prerequisites.postman_collection.json`;
-    const c2 = `${base}/collections/RFB-01_Create_Booking.postman_collection.json`;
-    const c3 = `${base}/collections/RFB-02_Retrieve_Booking.postman_collection.json`;
-    const c4 = `${base}/collections/RFB-03_Update_Booking.postman_collection.json`;
-    const c5 = `${base}/collections/RFB-04_Delete_Booking.postman_collection.json`;
-    const c6 = `${base}/environments/RFB_Environment.postman_environment.json`;
+    const collections = [
+      { file: 'RFB-00_Prerequisites.postman_collection.json',    env: envInitial },
+      { file: 'RFB-01_Create_Booking.postman_collection.json',   env: envFile },
+      { file: 'RFB-02_Retrieve_Booking.postman_collection.json', env: envFile },
+      { file: 'RFB-03_Update_Booking.postman_collection.json',   env: envFile },
+      { file: 'RFB-04_Delete_Booking.postman_collection.json',   env: envFile },
+    ];
 
-    await run(c1, envInitial);
-    await run(c2, envFile);
-    await run(c3, envFile);
-    await run(c4, envFile);
-    await run(c5, envFile);
-    await run(c6, envFile);
+    for (const { file, env } of collections) {
+      await run(`${base}/collections/${file}`, env);
+    }
 
     console.log('✔ Restful-booker finished');
   } catch (e) {
